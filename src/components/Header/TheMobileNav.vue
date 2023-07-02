febe332676310e94bf942b225a635e84de0151e7
Alexander Yadryshnikov <front.end3r@gmail.com>
2023-07-03 02:30:05 +0700

    Add two navs

---
+++ src/components/Header/TheMobileNav.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import {inject} from "vue";
+
+const navItems = inject('navItems')
+</script>
+
+<template>
+  <nav class="sticky bottom-0 z-50 bg-white lg:hidden">
+    <ul class="flex items-center justify-around border-t">
+      <li v-for="(icon, page) in navItems" class="flex-1" :key="page">
+        <a :href="`#${page}`" class="flex flex-col justify-center items-center p-2 text-xs capitalize">
+          <component class="h-6 w-6" :is="icon"/>
+          {{ page }}
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
