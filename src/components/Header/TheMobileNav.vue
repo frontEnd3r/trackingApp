8c5140bfed480b9bf26685f00fbd40604075e09b
Alexander Yadryshnikov <front.end3r@gmail.com>
2023-07-03 02:41:38 +0700

    Move mobile nav li items to new component

--- src/components/Header/TheMobileNav.vue
+++ src/components/Header/TheMobileNav.vue
@@ -1,18 +1,15 @@
 <script setup lang="ts">
 import {inject} from "vue";
-
+import MobileNavItem from "@/components/Header/MobileNavItem.vue";
 const navItems = inject('navItems')
 </script>
 
 <template>
   <nav class="sticky bottom-0 z-50 bg-white lg:hidden">
     <ul class="flex items-center justify-around border-t">
-      <li v-for="(icon, page) in navItems" class="flex-1" :key="page">
-        <a :href="`#${page}`" class="flex flex-col justify-center items-center p-2 text-xs capitalize">
-          <component class="h-6 w-6" :is="icon"/>
-          {{ page }}
-        </a>
-      </li>
+      <mobile-nav-item v-for="(icon, page) in navItems" :key="page" :href="`#${page}`">
+        <component class="h-6 w-6" :is="icon"/> {{ page }}
+      </mobile-nav-item>
     </ul>
   </nav>
 </template>
