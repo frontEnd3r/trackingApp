61e1bf76651fa4122dd5cebcd4031e69ce7ab6ad
Alexander Yadryshnikov <front.end3r@gmail.com>
2023-07-03 03:03:52 +0700

    Define pages from constants and set current page from hash after load

--- src/components/Header/TheMobileNav.vue
+++ src/components/Header/TheMobileNav.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
-import {inject} from "vue";
+import {inject, ref} from "vue";
 import MobileNavItem from "@/components/Header/MobileNavItem.vue";
-const navItems = inject('navItems')
+
+const navItems: MobileNavItem = inject('navItems')
+const currentPage: string = ref(window.location.hash.slice(1))
 </script>
 
 <template>
   <nav class="sticky bottom-0 z-50 bg-white lg:hidden">
     <ul class="flex items-center justify-around border-t">
-      <mobile-nav-item v-for="(icon, page) in navItems" :key="page" :href="`#${page}`">
-        <component class="h-6 w-6" :is="icon"/> {{ page }}
+      <mobile-nav-item
+          v-for="(icon, page) in navItems"
+          :key="page"
+          :href="`#${page}`"
+          :class="{'bg-gray-200 pointer-events-none': page === currentPage}"
+          @click="currentPage = page">
+        <component class="h-6 w-6" :is="icon"/>
+        {{ page }}
       </mobile-nav-item>
     </ul>
   </nav>
--- src/globalVariables.ts
+++ src/globalVariables.ts
@@ -1,10 +1,11 @@
 import {FunctionalComponent, provide} from 'vue'
 import {ChartBarIcon, ClockIcon, ListBulletIcon} from "@heroicons/vue/24/outline";
+import {PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS} from "@/contants";
 
 export const navItems: {
-    activities: FunctionalComponent
-    timeline: FunctionalComponent
-    progress: FunctionalComponent
+    [PAGE_ACTIVITIES]: FunctionalComponent
+    [PAGE_TIMELINE]: FunctionalComponent
+    [PAGE_PROGRESS]: FunctionalComponent
 } = {
     timeline: ClockIcon,
     activities: ListBulletIcon,
